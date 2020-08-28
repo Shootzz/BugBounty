aa67b6d414aa07ab5c80537c88f5ead6e2004119
Shootzz <cannburak9@gmail.com>
2020-08-28 14:21:26 +0300

    Create subfind.sh

---
+++ subfind.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	printf " --- Give the address as an argument\n For example: example.com\n"
+else
+        subfinder -d $1 -o subfinder_out
+	assetfinder $1 > assetfinder_out
+	python3 /root/opt/Sublist3r/sublist3r.py -d $1 -o sublister_out
+	cat subfinder_out assetfinder_out sublister_out | sort -u > domains.txt
+fi
