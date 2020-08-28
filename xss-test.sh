3eb79c1f52d0be5e3aabe39065a88046512135c2
Shootzz <cannburak9@gmail.com>
2020-08-28 14:31:42 +0300

    Create xss-test.sh

---
+++ xss-test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cat $1 | gau | egrep -v '(.css|.png|.jpeg|.jpg|.svg|.gif|.wolf)' | while read url; do vars=$(curl -s $url | grep -Eo "var [a-zA-Z0-9]+" | sed -e 's,'var','"$url"?',g' -e 's/ //g' | grep -v '.js' | sed 's/.*/&=xss/g'); echo -e "\e[1;33m$url\n\e[1;32m$vars" |tee -a xsstest.out | sort -u xsstest.out -o xsstest.out ;done
