c04e7ea248cb15d931123e3d687fe56756d237da
Shootzz <cannburak9@gmail.com>
2020-09-05 16:30:35 +0300

    Update subfind.sh

--- subfind.sh
+++ subfind.sh
@@ -7,4 +7,5 @@ else
 	assetfinder $1 > assetfinder_out
 	python3 /root/opt/Sublist3r/sublist3r.py -d $1 -o sublister_out
 	cat subfinder_out assetfinder_out sublister_out | sort -u > domains.txt
+	rm assetfinder_out subfinder_out sublister_out
 fi
